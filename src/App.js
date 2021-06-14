3d58ff36360c240ffa6562e9a7bbf2c3f07810da
Utkarsh Srivastav <utty@Utkarshs-MacBook-Air.local>
2021-06-14 14:20:21 +1000

    Updated protocol

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Login from "./Login";
 export const API_BASE_URL =
-  "https://sqltestservice-env.eba-c4ah2ern.ap-southeast-2.elasticbeanstalk.com";
+  "http://sqltestservice-env.eba-c4ah2ern.ap-southeast-2.elasticbeanstalk.com";
 function App() {
   return (
     <div className="App">
